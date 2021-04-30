86b56521a3eacfb5cadff935631302bf6986a689
dongleecsu <dongleecsu@gmail.com>
2021-04-30 22:54:06 +0800

    update nctugr set file

---
+++ run_data_collection.sh
@@ -0,0 +1,17 @@
+python dreamplace/Placer.py test/dac2012/superblue1.json
+python dreamplace/Placer.py test/dac2012/superblue2.json
+python dreamplace/Placer.py test/dac2012/superblue3.json
+python dreamplace/Placer.py test/dac2012/superblue4.json
+python dreamplace/Placer.py test/dac2012/superblue5.json
+python dreamplace/Placer.py test/dac2012/superblue6.json
+python dreamplace/Placer.py test/dac2012/superblue7.json
+python dreamplace/Placer.py test/dac2012/superblue9.json
+python dreamplace/Placer.py test/dac2012/superblue10.json
+python dreamplace/Placer.py test/dac2012/superblue11.json
+python dreamplace/Placer.py test/dac2012/superblue12.json
+python dreamplace/Placer.py test/dac2012/superblue14.json
+python dreamplace/Placer.py test/dac2012/superblue15.json
+python dreamplace/Placer.py test/dac2012/superblue16.json
+python dreamplace/Placer.py test/dac2012/superblue18.json
+
+
